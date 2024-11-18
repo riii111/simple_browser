1069faaa88d94ff3cb9c5fd7839f83d9f42e9323
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-11-18 12:19:58 +0900

    parse処理追加

--- core/src/url.rs
+++ core/src/url.rs
@@ -1,4 +1,5 @@
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
@@ -6,7 +7,7 @@ pub struct Url {
     host: String,
     port: String,
     path: String,
-    searchPart: String,
+    search_part: String,
 }
 
 impl Url {
@@ -16,7 +17,108 @@ impl Url {
             host: "".to_string(),
             port: "".to_string(),
             path: "".to_string(),
-            searchPart: "".to_string(),
+            search_part: "".to_string(),
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported.".to_string());
+        }
+
+        self.host = self.extract_host();
+        self.port = self.extract_port();
+        self.path = self.extract_path();
+        self.search_part = self.extract_searchpart();
+
+        Ok(self.clone())
+    }
+
+    fn is_http(&mut self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        false
+    }
+
+    pub fn host(&self) -> String {
+        self.host.clone()
+    }
+
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, '/')
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][..index].to_string() // 先頭からindexの文字(:)の手前まで
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+
+    pub fn port(&self) -> String {
+        self.port.clone()
+    }
+
+    fn extract_port(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, '/')
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][index + 1..].to_string()
+        } else {
+            "80".to_string()
+        }
+    }
+
+    pub fn path(&self) -> String {
+        self.path.clone()
+    }
+
+    fn extract_path(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, '/')
+            .collect();
+
+        // パスが存在しない（"example.com"など)
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, '?').collect();
+
+        path_and_searchpart[0].to_string()
+    }
+
+    pub fn search_part(&self) -> String {
+        self.search_part.clone()
+    }
+
+    fn extract_searchpart(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, '/')
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, '?').collect();
+
+        if path_and_searchpart.len() < 2 {
+            "".to_string()
+        } else {
+            path_and_searchpart[1].to_string()
         }
     }
 }
