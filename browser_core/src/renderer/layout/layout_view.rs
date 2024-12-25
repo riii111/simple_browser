7865d6e0bd2cd9a97df1db3d1a9fe93943b77407
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-12-25 12:10:32 +0900

    レイアウトの更新メソッドを追加

--- browser_core/src/renderer/layout/layout_view.rs
+++ browser_core/src/renderer/layout/layout_view.rs
@@ -1,11 +1,13 @@
+use crate::constants::CONTENT_AREA_WIDTH;
 use crate::renderer::css::cssom::StyleSheet;
 use crate::renderer::dom::api::get_target_element_node;
 use crate::renderer::dom::node::ElementKind;
 use crate::renderer::dom::node::Node;
-use crate::renderer::layout::layout_object::create_layout_object;
-use crate::renderer::layout::layout_object::LayoutObject;
+use crate::renderer::layout::layout_object::{LayoutObject, LayoutPoint, LayoutSize};
 use alloc::rc::Rc;
 use core::cell::RefCell;
+
+use super::layout_object::LayoutObjectKind;
 
 #[derive(Debug, Clone)]
 pub struct LayoutView {
@@ -30,6 +32,39 @@ impl LayoutView {
     pub fn root(&self) -> Option<Rc<RefCell<LayoutObject>>> {
         self.root.clone()
     }
+
+    fn update_layout(&mut self) {
+        Self::calculate_node_size(&self.root, LayoutSize::new(CONTENT_AREA_WIDTH, 0));
+
+        Self::calculate_node_position(
+            &self,
+            root,
+            LayoutPoint::new(0, 0),
+            LayoutObjectKind::Block,
+            None,
+            None,
+        )
+    }
+
+    fn calculate_node_size(node: &Option<Rc<RefCell<LayoutObject>>>, parent_size: LayoutSize) {
+        if let Some(n) = node {
+            // ノードがブロック要素の場合、子ノードのレイアウトを計算する前に横幅を決める
+            if n.borrow().kind() == LayoutObjectKind::Block {
+                n.borrow_mut().compute_size(parent_size); // ブロック要素は親の横幅いっぱいまで広がるので、親ノードの横幅と同等扱いでOK
+            }
+
+            let first_child = n.borrow().first_child();
+            Self::calculate_node_size(&first_child, n.borrow().size());
+
+            let next_sibling = n.borrow().next_sibling();
+            Self::calculate_node_size(&next_sibling, parent_size);
+
+            // 子ノードのサイズが決まってからサイズを計算する
+            // ブロック要素なら高さは子ノードに依存
+            // インライン要素なら高さも横幅も子ノードに依存
+            n.borrow_mut().compute_size(parent_size);
+        }
+    }
 }
 
 /// 再帰的に呼び出しレイアウトツリーを構築していく
@@ -42,13 +77,13 @@ fn build_layout_tree(
     // DOMノードに対応するレイアウトオブジェクトを生成
     // ただし"display:none"が指定されていた場合、ノードは生成しない
     let mut target_node = node.clone();
-    let mut layout_object = create_layout_object(node, parent_obj, cssom);
+    let mut layout_object = LayoutObject::create_layout_object(node, parent_obj, cssom);
 
     // ノードが生成されなかった場合、兄弟ノードを使用してレイアウトオブジェクトの生成を試みる（生成されるまで兄弟ノードを辿り続ける）
     while layout_object.is_none() {
         if let Some(n) = target_node {
             target_node = n.borrow().next_sibling().clone();
-            layout_object = create_layout_object(&target_node, parent_obj, cssom);
+            layout_object = LayoutObject::create_layout_object(&target_node, parent_obj, cssom);
         } else {
             // 兄弟ノードがなくなれば、処理すべきDOMツリーは終了したのでこれまでのレイアウトツリーを返す
             return layout_object;
