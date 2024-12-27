789554df3331d907ab5bb1bd05efb5db7c99dbdb
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-12-27 11:26:13 +0900

    ツールバーを描画

--- browser_core/src/constants.rs
+++ browser_core/src/constants.rs
@@ -19,3 +19,9 @@ pub static CHAR_HEIGHT: i64 = 16;
 pub static CHAR_HEIGHT_WITH_PADDING: i64 = CHAR_HEIGHT + 4;
 
 pub static WHITE: u32 = 0xffffff;
+pub static LIGHTGREY: u32 = 0xd3d3d3;
+pub static GREY: u32 = 0x808080;
+pub static DARKGREY: u32 = 0x5a5a5a;
+pub static BLACK: u32 = 0x000000;
+
+pub static ADDRESSBAR_HEIGHT: i64 = 20;
--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,10 +1,14 @@
 use alloc::rc::Rc;
 use browser_core::{
     browser::Browser,
-    constants::{WHITE, WINDOW_HEIGHT, WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS, WINDOW_WIDTH},
+    constants::{
+        ADDRESSBAR_HEIGHT, DARKGREY, LIGHTGREY, TOOLBAR_HEIGHT, WHITE, WINDOW_HEIGHT,
+        WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS, WINDOW_WIDTH,
+    },
 };
 use core::cell::RefCell;
-use noli::window::Window;
+use noli::error::Result as OsResult;
+use noli::window::{StringSize, Window};
 
 #[derive(Debug)]
 pub struct WasabiUI {
@@ -26,5 +30,63 @@ impl WasabiUI {
             )
             .unwrap(),
         }
+    }
+
+    fn setup_toolbar(&mut self) -> OsResult<()> {
+        // ツールバーの背景の四角を描画
+        self.window
+            .fill_rect(LIGHTGREY, 0, 0, WINDOW_WIDTH, TOOLBAR_HEIGHT)?;
+
+        // ツールバーのコンテンツエリアの境目の線を描画
+        self.window.draw_line(
+            DARKGREY,
+            0,
+            TOOLBAR_HEIGHT + 1,
+            WINDOW_WIDTH - 1,
+            TOOLBAR_HEIGHT + 1,
+        )?;
+
+        // アドレスバーの横に"Address:"という文字列を描画
+        self.window
+            .draw_string(BLACK, 5, 5, "Address:", StringSize::Medium, false)?;
+
+        // アドレスバーの四角を描画
+        self.window
+            .fill_rect(WHITE, 70, 2, WINDOW_WIDTH - 74, 2 + ADDRESSBAR_HEIGHT)?;
+
+        // アドレスバーの影の線を描画
+        self.window.draw_line(GREY, 70, 2, WINDOW_WIDTH - 4, 2)?;
+        self.window
+            .draw_line(GREY, 70, 2, 70, 2 + ADDRESSBAR_HEIGHT)?;
+        self.window.draw_line(BLACK, 71, 3, WINDOW_WIDTH - 5, 3)?;
+
+        Ok(())
+    }
+
+    pub fn setup(&mut self) -> Result<(), Error> {
+        if let Err(error) = self.setup_toolbar() {
+            // OsResultとResultが持つError型は異なるため、変換が必要
+            return Err(Error::InvalidUI(format!(
+                "failed to initialize a toolbar with error: {:#?}",
+                error
+            )));
+        }
+        // 画面を更新する
+        self.window.flush();
+
+        Ok(())
+    }
+
+    pub fn run_app(&mut self) -> Result<(), Error> {
+        // TODO: あとで実装
+        Ok(())
+    }
+
+    pub fn start(&mut self) -> Result<(), Error> {
+        self.setup()?;
+
+        self.run_app()?;
+
+        Ok(())
     }
 }
