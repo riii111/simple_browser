ad1bc0d7e8c5d911d3ef957f2d675c2f4c974b46
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-01-01 11:31:51 +0900

    JSレキサーの実装（簡易版）
    加算減算と変数・関数のみ

--- browser_core/src/renderer/js/README.md
+++ browser_core/src/renderer/js/README.md
@@ -1,8 +1,13 @@
-## 内容
+## 概要
 
-ここではJavaScriptのコードを解析し、実行する。
-ここではCSR（Client Side Rendering）、かつ以下の簡易機能のみを実装。
+JavaScriptエンジンを実装する。
 
-- 足し算・引き算
-- 変数の定義と使用
-- 関数の定義と呼び出し
+## 実装内容
+
+JavaScriptのコードを解析し、実行する。
+
+- JavaScriptエンジンにはインタプリタ、コンパイラ、JITコンパイラ（Just In Time Compiler）などの実装があるが、ここではインタプリタのみを実装する。
+- CSR（Client Side Rendering）、かつ以下の簡易機能のみを実装。
+  - 足し算・引き算
+  - 変数の定義と使用
+  - 関数の定義と呼び出し
--- browser_core/src/renderer/js/token.rs
+++ browser_core/src/renderer/js/token.rs
@@ -1,7 +1,90 @@
+use alloc::string::String;
+use alloc::string::ToString;
+use alloc::vec::Vec;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
     /// https://262.ecma-international.org/#sec-punctuators
     Punctuator(char),
     /// https://262.ecma-international.org/#sec-literals-numeric-literals
     Number(u64),
+}
+
+pub struct JsLexer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+/// レキサー: トークナイザーの同義語だが、トークナイザーの機能を含んだ、より広範な解析を行うコンポーネント
+/// - トークンの生成
+/// - トークンの分類
+/// - 行番号などの追加情報の付加など
+impl JsLexer {
+    pub fn new(input: &str) -> Self {
+        Self {
+            pos: 0,
+            input: input.chars().collect(),
+        }
+    }
+
+    /// 数字を解釈
+    fn consume_number(&mut self) -> u64 {
+        let mut num = 0;
+
+        loop {
+            if self.pos >= self.input.len() {
+                return num;
+            }
+
+            let c = self.input[self.pos];
+
+            match c {
+                '0'..='9' => {
+                    num = num * 10 + (c.to_digit(10).unwrap() as u64);
+                    self.pos += 1;
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+
+        num
+    }
+}
+
+/// 次のトークンを返すイテレータ
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        // ホワイトスペースまたは改行文字が続く限り、次の位置に進める
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.' => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                return Some(t);
+            }
+            '0'..='9' => Token::Number(self.consume_number()),
+            _ => {
+                unimplemented!("char {:?} is not supported yet", c);
+            }
+        };
+
+        Some(token)
+    }
 }
